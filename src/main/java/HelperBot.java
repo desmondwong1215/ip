217a659d1f849daa3a653f66c406558933190e51
desmondwong1215 <desmondwong1215@gmail.com>
2025-08-20 16:20:42 +0800

    Add support for echo

--- src/main/java/HelperBot.java
+++ src/main/java/HelperBot.java
@@ -1,22 +1,53 @@
+import java.util.Scanner;
+
 public class HelperBot {
 
     public static final String LINE = "____________________________________________________________";
+    public static final String NAME = "HelperBot";
 
-    public static void greet(String name) {
+    public void greet(String name) {
         System.out.println(HelperBot.LINE);
         System.out.printf("Hello! I'm %s.\n", name);
         System.out.println("What can I do for you?");
         System.out.println(HelperBot.LINE);
     }
 
-    public static void exit() {
+    public void echo(String message) {
+        System.out.println(HelperBot.LINE);
+        System.out.println(">> " + message);
+        System.out.println(HelperBot.LINE);
+    }
+
+    public void exit() {
+        System.out.println(HelperBot.LINE);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(HelperBot.LINE);
     }
 
+    public void chat() {
+        // greet the user
+        this.greet(HelperBot.NAME);
+
+        Scanner scanner = new Scanner(System.in);
+
+        // chat with the user, exit when user enter 'bye'
+        while (true) {
+            System.out.println();
+            String message = scanner.nextLine();
+
+            // user want to exit
+            if (message.equalsIgnoreCase("bye")) {
+                this.exit();
+                break;
+            }
+
+            // repeat the message entered by user
+            this.echo(message);
+        }
+    }
+
     public static void main(String[] args) {
-        String name = "HelperBot";
-        greet(name);
-        exit();
+        HelperBot bot = new HelperBot();
+        bot.chat();
     }
 }
