2d52efe54977b26c4ae1995e1eee9251c6a62874
desmondwong1215 <desmondwong1215@gmail.com>
2025-09-17 17:33:35 +0800

    Add title bar and application icon

--- src/main/java/helperbot/ui/Main.java
+++ src/main/java/helperbot/ui/Main.java
@@ -1,11 +1,13 @@
 package helperbot.ui;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -26,6 +28,9 @@ public class Main extends Application {
             stage.setScene(scene);
             stage.setMinHeight(220);
             stage.setMinWidth(417);
+            stage.setTitle("HelperBot");
+            stage.getIcons().add(new Image(Objects.requireNonNull(
+                    this.getClass().getResourceAsStream("/images/HelperBot.jpeg"))));
             fxmlLoader.<MainWindow>getController().setHelperBot(this.helperBot);
             fxmlLoader.<MainWindow>getController().setStage(stage);
             stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
