de1d1a147dcfb6fd224a042c79e21b8ef995209d
desmondwong1215 <desmondwong1215@gmail.com>
2025-09-17 16:49:39 +0800

    Improve the GUI

--- src/main/java/helperbot/ui/Main.java
+++ src/main/java/helperbot/ui/Main.java
@@ -24,6 +24,8 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
             fxmlLoader.<MainWindow>getController().setHelperBot(this.helperBot);
             fxmlLoader.<MainWindow>getController().setStage(stage);
             stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
